e5ca0b6a3d80e9e6d75132d11b26f52ebe228ff2
glowiingo <gngo2@my.bcit.ca>
2020-04-01 18:46:59 -0700

    Revert "add gmap link"
    This reverts commit b25814a774f2a37fdbbb21b169dccc4020761113.

--- app/src/main/java/ca/bcit/coveropspainters/MainActivity.java
+++ app/src/main/java/ca/bcit/coveropspainters/MainActivity.java
@@ -23,7 +23,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button signIn = findViewById(R.id.signIn);
         Button current = findViewById(R.id.current);
-        Button google_map = findViewById(R.id.gmaps_button);
 
         signIn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,13 +36,6 @@ public class MainActivity extends AppCompatActivity {
                 current_intent(v);
             }
         });
-        google_map.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                gmap(v);
-            }
-        });
-
     }
 
     public void sign_in(View v) {
