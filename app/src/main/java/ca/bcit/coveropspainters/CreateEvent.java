1f1cfca0929dc21f71b331c6b320e170d65e6780
flemming chow <flemming@flemmings-MacBook-Pro.local>
2020-04-03 00:45:10 -0700

    created CreateEvent activites and joined it with list of activiteis

--- app/src/main/java/ca/bcit/coveropspainters/CreateEvent.java
+++ app/src/main/java/ca/bcit/coveropspainters/CreateEvent.java
@@ -3,12 +3,20 @@ package ca.bcit.coveropspainters;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class CreateEvent extends AppCompatActivity {
+
+    TextView eventData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
+
+        eventData = (TextView)findViewById(R.id.eventDescription);
+        Bundle bundle = getIntent().getExtras();
+        
+        eventData.
     }
 }
