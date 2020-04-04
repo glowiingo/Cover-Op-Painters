89ff76eb63c1d59015884603f2090a64012c6289
flemming chow <flemming@flemmings-MacBook-Pro.local>
2020-04-03 23:16:30 -0700

    adjusted createevent activity layout

--- app/src/main/java/ca/bcit/coveropspainters/CreateEvent.java
+++ app/src/main/java/ca/bcit/coveropspainters/CreateEvent.java
@@ -26,18 +26,14 @@ public class CreateEvent extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
-
         addressData = (TextView)findViewById(R.id.addressData);
         cityData = (TextView)findViewById(R.id.cityData);
         postalData = (TextView)findViewById(R.id.postalCodeData);
-
         geocoder = new Geocoder(this, Locale.getDefault());
-
         Bundle bundle = getIntent().getExtras();
         String dataArea =  bundle.getString("area");
         String lat =  bundle.getString("lat");
         String lng =  bundle.getString("lng");
-
         try {
             double dataLat = Double.parseDouble(lng);
             double dataLng = Double.parseDouble(lat);
@@ -45,15 +41,12 @@ public class CreateEvent extends AppCompatActivity {
             Log.d(TAG, "onCreate longitude: " + dataLat);
             addresses = geocoder.getFromLocation(dataLat, dataLng, 1);
             if (addresses.size()>0){
-//                String address = addresses.get(0).getAddressLine(0);
                 StringBuilder str = new StringBuilder();
-
                 String street = addresses.get(0).getThoroughfare();
                 String num = addresses.get(0).getSubThoroughfare();
                 str.append(num + " " + street);
                 String city = addresses.get(0).getLocality();
                 String postalCode = addresses.get(0).getPostalCode();
-
                 addressData.setText(str);
                 cityData.setText(city);
                 postalData.setText(postalCode);
