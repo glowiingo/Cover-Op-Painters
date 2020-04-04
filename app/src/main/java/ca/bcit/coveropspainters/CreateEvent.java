4e4f7c4dd22e4653408781495a2b98f95101b3f8
flemming chow <flemming@flemmings-MacBook-Pro.local>
2020-04-03 20:30:06 -0700

    testing reverese geocoding

--- app/src/main/java/ca/bcit/coveropspainters/CreateEvent.java
+++ app/src/main/java/ca/bcit/coveropspainters/CreateEvent.java
@@ -5,19 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
 public class CreateEvent extends AppCompatActivity {
 
+    private static final String TAG = "CreateEventActivity";
     TextView addressData;
     TextView postalData;
     TextView cityData;
-//    Geocoder geocoder = new Geocoder(this, Locale.getDefault());
-//    List<Address> adList;
+    Geocoder geocoder;
+    ArrayList<Address> addresses;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,25 +31,34 @@ public class CreateEvent extends AppCompatActivity {
         cityData = (TextView)findViewById(R.id.cityData);
         postalData = (TextView)findViewById(R.id.postalCodeData);
 
+        geocoder = new Geocoder(this, Locale.getDefault());
+
         Bundle bundle = getIntent().getExtras();
         String dataType =  bundle.getString("type");
-        String dataLat =  bundle.getString("lat");
-        String dataLng =  bundle.getString("lng");
-        addressData.setText(dataLat);
+        String lat =  bundle.getString("lat");
+        String lng =  bundle.getString("lng");
+        Log.d(TAG, "onCreate longitude: " + lng);
+        Log.d(TAG, "onCreate latitude: " + lat);
+
+
 //        try {
-//            adList = geocoder.getFromLocation(Double.parseDouble(dataLat), Double.parseDouble(dataLng), 1);
+//            double dataLat = Double.parseDouble(lat);
+//            double dataLng = Double.parseDouble(lng);
+//            Log.d(TAG, "onCreate latitude: " + dataLat);
+//            Log.d(TAG, "onCreate longitude: " + dataLat);
+//            addresses = (ArrayList<Address>) geocoder.getFromLocation(dataLat, dataLng, 1);
+//            if (addresses.size()>0){
+//                String address = addresses.get(0).getAddressLine(0);
+//                String city = addresses.get(0).getLocality();
+//                String postalCode = addresses.get(0).getPostalCode();
 //
-//            String address = adList.get(0).getAddressLine(0);
-////            String city = adList.get(0).getLocality();
-////            String postalCode = adList.get(0).getPostalCode();
-//
-//            addressData.setText(address);
-////            cityData.setText(city);
-////            postalData.setText(postalCode);
-//        } catch (IOException e) {
-//            e.printStackTrace();
+//                addressData.setText(address);
+//                cityData.setText(city);
+//                postalData.setText(postalCode);
+//            }
 //        }
-
-
+//        catch (NullPointerException | IOException e){
+//            e.getStackTrace();
+//        }
     }
 }
